c5d81ce4a346c20d6433d7f7491961aa3ffebf8e
xincoder <xincoder@gmail.com>
2020-04-10 16:35:50 -0700

    add file_path at header and page_number at footer

--- pdf_code.py
+++ pdf_code.py
@@ -9,26 +9,26 @@ import pygments
 from pygments import lexers, formatters, styles
 import shutil
 
-
 __version__ = '1.0.0'
 
-class Code2pdf:
+class PDF_Code:
     """
         Convert a source file into a pdf with syntax highlighting.
     """
-    def __init__(self, input_file=None, output_file=None, size="A4", margin=0.4):
+    def __init__(self, input_file=None, output_file=None, size="A4", margin=0.4, input_root=''):
         if not input_file:
             raise Exception("input file is required")
         self.input_file = input_file
-        self.pdf_file = output_file #or "{}.pdf".format(input_file.split('.')[0])
+        self.pdf_file = output_file 
         self.size = size
         self.margin = '{}in'.format(margin)
+        self.input_root = input_root
 
         now_folder = os.path.dirname(output_file)
         if not os.path.exists(now_folder):
             os.makedirs(now_folder)
 
-    def highlight_file(self, linenos=True, style='default'):
+    def highlight_code(self, linenos=True, style='default'):
         """ Highlight the input file, and return HTML as a string. """
         try:
             lexer = lexers.get_lexer_for_filename(self.input_file)
@@ -64,7 +64,7 @@ class Code2pdf:
         print('Processing {} ...'.format(self.input_file))
         
         if convert:
-            text = self.highlight_file(linenos=linenos, style=style)
+            text = self.highlight_code(linenos=linenos, style=style)
             options = {
                 'page-size': self.size.lower(),
                 'margin-top': self.margin,
@@ -74,7 +74,13 @@ class Code2pdf:
                 'encoding': "UTF-8",
                 'custom-header' : [
                     ('Accept-Encoding', 'gzip')
-                ]
+                ],
+                # 'header-left':self.input_file.replace(self.input_root, './'),
+                'header-left':self.input_file,
+                'header-font-size': 7,
+                'footer-center': '[page]',
+                'footer-font-size': 7,
+                
             }
             pdfkit.from_string(input=text, output_path=self.pdf_file, cover='', options=options)
         else:
@@ -83,37 +89,20 @@ class Code2pdf:
         print('####################################################')
 
 
-# def get_output_file(inputname, outputfolder=None):
-#     """ If the output name is set, then return it.
-#         Otherwise, build an output name using the current directory,
-#         replacing the input name's extension.
-#     """
-#     input_root = os.path.dirname(inputname)
-#     if outputfolder:
-#         # return os.path.join(input_root, outputfolder)
-#         outputpath = inputname.replace(input_root, outputfolder)
-
-#     file_name, file_ext = os.path.splitext(inputname)
-
-#     outputpath = inputname.replace(file_ext, '.pdf')  
-#     return outputpath
     
 def get_path_list(path_src, path_dst):
     input_root = path_src
-
     input_file_list = []
-    # output_file_list = []
+
     if os.path.isfile(path_src):
         input_file_list.append(path_src)    
         input_root = os.path.dirname(path_src)+'/'
-
-        # file_name, file_ext = os.path.splitext(path_src)
-        # output_file_list.append(path_src.replace(file_ext, '.pdf'))
     else:
         input_root = os.path.dirname(input_root+'/')+'/'
         for (current_path, subfolder, filenames) in os.walk(path_src):
             input_file_list += [os.path.join(current_path, x) for x in filenames]
 
+    
     if path_dst is None:
         path_dst = os.path.join(input_root, 'Pdf_Code_Results/')
     else:
@@ -128,24 +117,11 @@ def get_path_list(path_src, path_dst):
     # replace extension
     # now_ext_list = [os.path.splitext(x)[-1] for x in now_file_list]
     # output_file_list = [name.replace(ext, '.pdf') if ext else name for name, ext in zip(now_file_list, now_ext_list)] 
-    
+
+    # convert_mask_list = [magic.detect_from_filename(x).encoding!='binary' for x in input_file_list]    
     output_file_list = [name+'.pdf' if convert_mask else name for name, convert_mask in zip(now_file_list, convert_mask_list) ] 
-    # convert_mask_list = [magic.detect_from_filename(x).encoding!='binary' for x in input_file_list]
     
-    
-
-    # encod = magic.detect_from_filename(input_file_list[0]).encoding
-    # print(encod, type(encod))
-    # # infile_list, outfile_list = get_path_list(args.src, args.dst)
-    # # for in_path in infile_list:
-    #     detected = magic.detect_from_filename(in_path)
-    #     print(in_path)
-    #     print(detected.mime_type)
-    #     print(detected.encoding)
-    #     print(detected.name)
-    #     print()
-
-    return input_file_list, output_file_list, convert_mask_list
+    return input_file_list, output_file_list, convert_mask_list, input_root
 
 
 def parse_arg():
@@ -197,20 +173,11 @@ def parse_arg():
     return parser.parse_args()
 
 
-
-
 def main():
     args = parse_arg()
-    # pdf_file = get_output_file(args.filename, args.outputfolder)
-    # pdf = Code2pdf(args.filename, pdf_file, args.size, args.margin)
-    # pdf = Code2pdf(ifile=args.filename, pdf_file, args.size, args.margin)
-
-    infile_list, outfile_list, convert_mask_list = get_path_list(args.src, args.dst)
+    infile_list, outfile_list, convert_mask_list, input_root = get_path_list(args.src, args.dst)
     for in_path, out_path, convert_mask in zip(infile_list, outfile_list, convert_mask_list):
-        # print(in_path, convert_mask)
-        # print(out_path)
-        # print()
-        pdf = Code2pdf(input_file=in_path, output_file=out_path, size=args.size, margin=args.margin)
+        pdf = PDF_Code(input_file=in_path, output_file=out_path, size=args.size, margin=args.margin, input_root=input_root)
         pdf.save_pdf(linenos=args.linenos, style=args.style, convert=convert_mask)
     
     return 0
